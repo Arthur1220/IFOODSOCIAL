a9ed61ce4fd2bce4a3262a2738464c42069f8fb0
Arthur <arthurmarquesazevedo@gmail.com>
2024-06-17 16:49:47 -0300

    Fix: Layout fix

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -16,18 +16,5 @@ defineExpose(components)
 </template>
 
 <style scoped>
-.app-container {
-  width: 100%;
-  min-height: 100vh; /* Garante que o contêiner ocupe pelo menos a altura total da tela */
-  display: flex;
-  flex-direction: column;
-  align-items: center; /* Alinha os itens ao centro */
-}
 
-@media (min-width: 768px) {
-  .app-container {
-    max-width: 1200px; /* Define um valor máximo para telas maiores */
-    margin: 0 auto; /* Centraliza o contêiner na tela */
-  }
-}
 </style>
